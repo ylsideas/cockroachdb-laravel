65870fc5f7b2dc4332527555150442ad3d3b649f
Peter Fox <peter.fox@peterfox.me>
2022-02-12 17:51:24 +0000

    lets go

--- tests/Database/DatabaseCockroachDbQueryBuilderTest.php
+++ tests/Database/DatabaseCockroachDbQueryBuilderTest.php
@@ -125,6 +125,10 @@ class DatabaseCockroachDbQueryBuilderTest extends TestCase
 
     public function testWhereFullTextThrowsExceptionCockroachDb()
     {
+        if (! method_exists(Grammar::class, 'whereFulltext')) {
+            $this->markTestSkipped('fullText features do not exist in this application version');
+        }
+        
         $this->expectException(FeatureNotSupportedException::class);
         $builder = $this->getCockroachDbBuilder();
         $builder->select('*')->from('users')->whereFullText('description', 'should contain');
