ea156cee927aea5f279953051e8e202f5b25421b
Peter Fox <peter.fox@peterfox.me>
2022-02-12 13:46:07 +0000

    Improve Cockroach from using too many resources

--- database.php
+++ database.php
@@ -20,6 +20,17 @@ try {
 CREATE DATABASE IF NOT EXISTS forge;
 CREATE USER IF NOT EXISTS forge;
 GRANT ALL ON DATABASE forge TO forge;
+
+SET CLUSTER SETTING kv.raft_log.disable_synchronization_unsafe = true;
+SET CLUSTER SETTING kv.range_merge.queue_interval = '50ms';
+SET CLUSTER SETTING jobs.registry.interval.gc = '30s';
+SET CLUSTER SETTING jobs.registry.interval.cancel = '180s';
+SET CLUSTER SETTING jobs.retention_time = '15s';
+SET CLUSTER SETTING schemachanger.backfiller.buffer_increment = '128 KiB';
+SET CLUSTER SETTING sql.stats.automatic_collection.enabled = false;
+SET CLUSTER SETTING kv.range_split.by_load_merge_delay = '5s';
+ALTER RANGE default CONFIGURE ZONE USING "gc.ttlseconds" = 5;
+ALTER DATABASE system CONFIGURE ZONE USING "gc.ttlseconds" = 5;
 heredoc);
 } catch (PDOException $exception) {
     exit('Failed to creating database: ' . $exception->getMessage() . PHP_EOL);
