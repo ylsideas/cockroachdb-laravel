420e6facc3b72fbf6675d1dbde8aff26728cdad2
Vin Souza <vin@8sistemas.com>
2022-09-22 19:57:30 -0300

    chore: database connector with cluster options support

--- src/CockroachDbConnector.php
+++ src/CockroachDbConnector.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Connectors\PostgresConnector;
 
 class CockroachDbConnector extends PostgresConnector implements ConnectorInterface
 {
+    /**
+     * Usually the normal PostgresConnector would suffice for Cockroach,
+     * but Cockroach Serverless Clusters need an extra parameter `options`.
+     */
+    protected function getDsn(array $config)
+    {
+        $dsn = parent::getDsn($config);
+
+        if (isset($config['cluster']) && !empty($config['cluster'])) {
+            $dsn .= ";options='--cluster={$config['cluster']}'";
+        }
+
+        return $dsn;
+    }
 }
---
+++ tests/Database/DatabaseCockroachDbConnectorTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace YlsIdeas\CockroachDb\Tests\Database;
+
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use YlsIdeas\CockroachDb\CockroachDbConnector;
+
+class DatabaseCockroachDbConnectorTest extends TestCase
+{
+    public function test_dsn_params_with_cluster()
+    {
+        $connector = $this->getConnector();
+
+        $dsnConfig = $this->callProtectedOrPrivateMethod($connector, 'getDsn', [
+            [
+                'host' => 'localhost',
+                'database' => 'defaultdb',
+                'port' => '23456',
+                'cluster' => 'cluster-1234'
+            ]
+        ]);
+
+        $this->assertEquals("pgsql:host=localhost;dbname='defaultdb';port=23456;options='--cluster=cluster-1234'", $dsnConfig);
+    }
+
+    public function test_dsn_params_without_cluster()
+    {
+        $connector = $this->getConnector();
+
+        $dsnConfig = $this->callProtectedOrPrivateMethod($connector, 'getDsn', [
+            [
+                'host' => 'localhost',
+                'database' => 'defaultdb',
+                'port' => '23456',
+                'cluster' => ''
+            ]
+        ]);
+
+        $this->assertEquals("pgsql:host=localhost;dbname='defaultdb';port=23456", $dsnConfig);
+    }
+
+    /**
+     * Some methods might be protected and need to be tested. So this just exposes them using reflection.
+     */
+    protected function callProtectedOrPrivateMethod($object, string $methodName, array $arguments = [])
+    {
+        $reflectionClass = new ReflectionClass($object);
+        $reflectionMethod = $reflectionClass->getMethod($methodName);
+        $reflectionMethod->setAccessible(true);
+
+        return empty($arguments) ?
+            $reflectionMethod->invoke($object) :
+            $reflectionMethod->invokeArgs($object, $arguments);
+    }
+
+    protected function getConnector()
+    {
+        return new CockroachDbConnector();
+    }
+}
