ba05b63f82ab363b81d35e9b5323f8e7a605209a
Vin <vin@8sistemas.com>
2022-09-26 19:55:38 -0300

    Add support for Cockroach Serverless Cluster identifier (#11)
    * chore: database connector with cluster options support
    
    * docs: add sample snippet in README for the cluster option
    
    * Fix PHP styling
    
    * fix: laravel 8 dsn quotes
    
    * chore: PR code review fixes
    
    * Fix PHP styling
    
    Co-authored-by: tryvin <tryvin@users.noreply.github.com>

--- src/CockroachDbConnector.php
+++ src/CockroachDbConnector.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Connectors\PostgresConnector;
 
 class CockroachDbConnector extends PostgresConnector implements ConnectorInterface
 {
+    /**
+     * Usually the normal PostgresConnector would suffice for Cockroach,
+     * but Cockroach Serverless Clusters need an extra parameter `options`.
+     */
+    protected function getDsn(array $config)
+    {
+        return $this->addClusterOptions(parent::getDsn($config), $config);
+    }
+
+    protected function addClusterOptions(string $dsn, array $config)
+    {
+        if (isset($config['cluster']) && ! empty($config['cluster'])) {
+            $clusterNameEscaped = addslashes($config['cluster']);
+            $dsn .= ";options='--cluster={$clusterNameEscaped}'";
+        }
+
+        return $dsn;
+    }
 }
---
+++ tests/Database/DatabaseCockroachDbConnectorTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace YlsIdeas\CockroachDb\Tests\Database;
+
+use PHPUnit\Framework\TestCase;
+use YlsIdeas\CockroachDb\CockroachDbConnector;
+
+class DatabaseCockroachDbConnectorTest extends TestCase
+{
+    public function test_dsn_params_with_cluster()
+    {
+        $connector = $this->getConnector();
+
+        $dsnConfig = $connector->exposeGetDsnMethod(
+            [
+                'host' => 'localhost',
+                'database' => 'defaultdb',
+                'port' => '23456',
+                'cluster' => 'cluster-1234',
+            ],
+        );
+
+        $this->assertStringContainsString("options='--cluster=cluster-1234'", $dsnConfig);
+    }
+
+    public function test_dsn_params_without_cluster()
+    {
+        $connector = $this->getConnector();
+
+        $dsnConfig = $connector->exposeGetDsnMethod(
+            [
+                'host' => 'localhost',
+                'database' => 'defaultdb',
+                'port' => '23456',
+                'cluster' => '',
+            ],
+        );
+
+        $this->assertStringNotContainsString("options=", $dsnConfig);
+    }
+
+    protected function getConnector()
+    {
+        return new class () extends CockroachDbConnector {
+            public function exposeGetDsnMethod(array $config)
+            {
+                return $this->getDsn($config);
+            }
+        };
+    }
+}
