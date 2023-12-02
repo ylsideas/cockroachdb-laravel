58055c22c1db32ad8c194e4c793dd03bffaf89fa
Tom Westrick <tjwestrick@gmail.com>
2023-12-02 18:34:02 -0500

    fix: Any artisan command that drops tables erroring with unknown function: pg_total_relation_size() (#26)
    * Override compileTables setting table size to 0
    
    * Change default size to -1 to make it apparent that it is not supported
    
    * Change all quotes to single
    
    Co-authored-by: Peter Fox <peter.fox@peterfox.me>
    
    ---------
    
    Co-authored-by: Peter Fox <peter.fox@peterfox.me>

--- src/Schema/CockroachGrammar.php
+++ src/Schema/CockroachGrammar.php
@@ -10,6 +10,23 @@ use YlsIdeas\CockroachDb\Exceptions\FeatureNotSupportedException;
 class CockroachGrammar extends PostgresGrammar
 {
     /**
+     * Compile the query to determine the tables.
+     *
+     * CockroachDB doesn't yet support pg_total_relation_size()
+     * https://github.com/cockroachdb/cockroach/issues/20712
+     * https://github.com/cockroachdb/cockroach/pull/59604
+     *
+     * @return string
+     */
+    public function compileTables()
+    {
+        return 'select c.relname as name, n.nspname as schema, -1 as size, '
+            . 'obj_description(c.oid, \'pg_class\') as comment from pg_class c, pg_namespace n '
+            . 'where c.relkind = \'r\' and n.oid = c.relnamespace '
+            . 'order by c.relname';
+    }
+
+    /**
      * Compile a fulltext index key command.
      *
      * @param  \Illuminate\Database\Schema\Blueprint  $blueprint
