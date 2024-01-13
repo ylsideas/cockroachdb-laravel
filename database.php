f3383bb6323012ea374a3881f31da275ff702ad2
Peter Fox <peter.fox@peterfox.me>
2024-01-13 00:57:42 +0000

    Add crdb v23 (#29)
    * Add crdb v23
    
    * deprecate old crdb versions
    
    * update composer keywords
    
    * fix database script for Crdb v23

--- database.php
+++ database.php
@@ -22,17 +22,24 @@ DROP USER IF EXISTS forge;
 CREATE DATABASE forge;
 CREATE USER forge;
 GRANT ALL PRIVILEGES ON DATABASE forge TO forge;
-
-SET CLUSTER SETTING kv.raft_log.disable_synchronization_unsafe = true;
-SET CLUSTER SETTING kv.range_merge.queue_interval = '50ms';
-SET CLUSTER SETTING jobs.registry.interval.gc = '30s';
-SET CLUSTER SETTING jobs.registry.interval.cancel = '180s';
-SET CLUSTER SETTING jobs.retention_time = '15s';
-SET CLUSTER SETTING sql.stats.automatic_collection.enabled = false;
-SET CLUSTER SETTING kv.range_split.by_load_merge_delay = '5s';
-ALTER RANGE default CONFIGURE ZONE USING "gc.ttlseconds" = 5;
-ALTER DATABASE system CONFIGURE ZONE USING "gc.ttlseconds" = 5;
 heredoc);
+
+    $statements = [
+        'SET CLUSTER SETTING kv.raft_log.disable_synchronization_unsafe = true;',
+        'SET CLUSTER SETTING kv.range_merge.queue_interval = \'50ms\';',
+        'SET CLUSTER SETTING jobs.registry.interval.gc = \'30s\';',
+        'SET CLUSTER SETTING jobs.registry.interval.cancel = \'180s\';',
+        'SET CLUSTER SETTING jobs.retention_time = \'15s\';',
+        'SET CLUSTER SETTING sql.stats.automatic_collection.enabled = false;',
+        'SET CLUSTER SETTING kv.range_split.by_load_merge_delay = \'5s\';',
+        'ALTER RANGE default CONFIGURE ZONE USING "gc.ttlseconds" = 5;',
+        'ALTER DATABASE system CONFIGURE ZONE USING "gc.ttlseconds" = 5;',
+    ];
+
+    foreach ($statements as $statement) {
+        $pdo->exec($statement);
+    }
+
 } catch (PDOException $exception) {
     exit('Failed to creating database: ' . $exception->getMessage() . PHP_EOL);
 }
